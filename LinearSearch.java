74dce4a1a8b4bc35da8fd4bf7cbfbce0c38edbdd
HITZ <hitesharma11111@gmail.com>
2025-07-11 16:42:50 +0530

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,3 +1,5 @@
+//Write a Java program to perform a linear search to find a target element in an array of integers. Return the index of the target element if found, otherwise return -1.
+
 public int linearSearch(int[] arr, int target) {
     for (int i = 0; i < arr.length; i++) {
         if (arr[i] == target) {
